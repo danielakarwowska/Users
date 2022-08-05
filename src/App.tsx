601561ca56632e724dd4facd35121b9a3a89ac9c
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-08-05 13:41:11 +0200

    adding json server

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,48 @@ import Table from './components/table'
 import React,{ useState } from 'react'
 import { useEffect } from 'react'
 
-const API_HOST = "http://localhost:3000"
-const USERS_API_URL = `${API_HOST}/users`
 
 const App = () => {
-
-    const [data,setData] = useState([])
+    // const API_HOST = "http://localhost:3000"
+    // const USERS_API_URL = `${API_HOST}/users`
     
-    const fetchUsers = () => {
-        fetch (`${USERS_API_URL}`)
-        .then(res => res.json())
-        .then(json => setData(json))
-    }
+     const [data,setData] = useState([])
+    
+    // const fetchUsers = () => {
+    //     fetch(`${USERS_API_URL}`)
+    //     .then(res => res.json())
+    //     .then(json => setData(json))
+    // }
 
-    useEffect(() => {
-        fetchUsers()
-    },[])
+    // useEffect(() => {
+    //     fetchUsers()
+    // },[])
 
     return(
-        <>
-        <Table/>
-        {
-            data.map ((item) => {
-                <tr key={item.name}>
-                    <td>{item.uri}</td>
-                    <td>{item.email}</td>
+        <div className="container">
+        <h1>Simple Users Table</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Company</th>
+                <th>User</th>
 
-                </tr>
-            })
-        }
-        </>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    data.map((item) => (
+                        <tr key={item.name}>
+                            <td>{item.product_name}</td>
+                            <td>{item.uri}</td>
+                            <td>{item.mail}</td>
+                            <td/>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+    </div>
     )
 }
 export default App
